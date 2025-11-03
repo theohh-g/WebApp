e3c5aea843d1514e13c64cfa3e2384927a87edca
Dillon Cool <dilloncool25@gmail.com>
2025-11-03 12:57:39 -0600

    commit

--- obj/Debug/net9.0/WebApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/WebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2632f29ae7f785a2e2e1ccbb03f4c5b3c209267f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
